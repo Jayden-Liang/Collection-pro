361f223a05ca76b27d1ae97d815fb46ccc27ad4d
Jayden-Liang <liangjisong@foxmail.com>
2019-08-29 22:18:11 +0800

    /ingre

--- project/blueprints/others/views.py
+++ project/blueprints/others/views.py
@@ -1,5 +1,6 @@
 from flask import redirect, render_template, Blueprint, request, flash, current_app, url_for, flash, jsonify
 from pymongo import MongoClient
+from project.extensions import  csrf
 import json
 
 Ingre= Blueprint('Ingre', __name__, template_folder='templates', url_prefix='/ingre' )
@@ -7,15 +8,21 @@ Ingre= Blueprint('Ingre', __name__, template_folder='templates', url_prefix='/in
 client = MongoClient('mongodb://mongo:27017')
 db = client.Burger
 Ingres = db['Ingres']
-Ingres.insert({
-    "meat":0,
-    "cheese":0,
-    "salad":0
-
-})
+# Ingres.insert({
+#     "meat":0,
+#     "cheese":0,
+#     "salad":0
+#
+# })
 @Ingre.route('/', methods=['POST','GET'])
+@csrf.exempt
 def index():
-    if request.method=="Post":
+    if request.method=="POST":
         data= request.get_json()
+        print(data)
+        Ingres.insert(data)
         return jsonify('i get it')
-    return jsonify(Ingres.find({},{'_id':0})[0])
+    a={}
+    for x in Ingres.find({},{'_id':0}):
+        a.update(x)
+    return jsonify(a)
